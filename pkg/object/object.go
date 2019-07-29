b8911bc94d201561156d061f855950f87c8b0c7a
Conrad Christensen <conrad.christensen@gmail.com>
2019-07-29 16:07:06 +0200

    working on evaluator

---
+++ pkg/object/object.go
@@ -0,0 +1,46 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (*Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+
+type Boolean struct {
+	Value bool
+}
+
+func (*Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct {}
+
+func (*Null) Type() ObjectType { return NULL_OBJ }
+func (*Null) Inspect() string { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go.smartmachine.io/cumulus/pkg/evaluator"
 	"go.smartmachine.io/cumulus/pkg/lexer"
 	"go.smartmachine.io/cumulus/pkg/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
