ad7245eec262b5a83aefeb659d9c19f3b9d1a7a4
Conrad Christensen <conrad.christensen@gmail.com>
2019-08-01 14:56:43 +0200

    Adding compiler

---
+++ pkg/compiler/compiler.go
@@ -0,0 +1,65 @@
+package compiler
+
+import (
+	"go.smartmachine.io/cumulus/pkg/ast"
+	"go.smartmachine.io/cumulus/pkg/code"
+	"go.smartmachine.io/cumulus/pkg/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		// TODO: What now?!
+	}
+
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
